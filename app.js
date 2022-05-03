7a8f5c3c12f0ecc554a69199be7b1c61ff97dfb8
aditechdev <aditechdevofficial@gmail.com>
2022-05-03 14:20:56 +0530

    :sparkles: Middle ware added for login

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 // Initialise the app
 const app = express();
 // add port-> this is the number where app will run
-
 const port = 3003;
+// add middle ware
+const middleware = require("./middleware");
+
+
 const server = app.listen(port, () => console.log("Server linstening on port: " + port));
 
 // Useing Template Engine "pug"
@@ -12,7 +15,7 @@ app.set("view engine", "pug");
 app.set("views", "views");
 
 // To send get request
-app.get("/", (req, res, next) => { 
+app.get("/", middleware.requireLogin, (req, res, next) => { 
     // added payLoad
     var payload = {
         pageTitle: "Home"
