d35bf9a47921f584f6e9e35dd7fdb1eeb8add8b3
aditechdev <aditechdevofficial@gmail.com>
2022-05-03 12:34:08 +0530

    :sparkles: Install pug and get request

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ const app = express();
 // add port-> this is the number where app will run
 
 const port = 3003;
+const server = app.listen(port, () => console.log("Server linstening on port: " + port));
+
+// To send get request
+app.get("/", (req, res, next) => { 
+    res.status(200).send("Yahooo!");
+})
