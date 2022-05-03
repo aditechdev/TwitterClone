5cd807e1ddbf97ea57eff85bddcb8b7fed1bcc31
aditechdev <aditechdevofficial@gmail.com>
2022-05-04 02:28:23 +0530

    :bug: Password verify fix and register new screen

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const port = 3003;
 // add middle ware
 const middleware = require("./middleware");
 const path = require("path");
+const bodyParser = require("body-parser");
+
+// BootStrap 4.4
+// https://getbootstrap.com/docs/4.4/getting-started/introduction/
 
 
 const server = app.listen(port, () => console.log("Server linstening on port: " + port));
@@ -14,6 +18,7 @@ const server = app.listen(port, () => console.log("Server linstening on port: " 
 // Useing Template Engine "pug"
 app.set("view engine", "pug");
 app.set("views", "views");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // ROutes
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -4,10 +4,12 @@ const express = require("express");
 const app = express();
 
 const router = express.Router();
+const bodyParser = require("body-parser");
 
 // Useing Template Engine "pug"
 app.set("view engine", "pug");
 app.set("views", "views");
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // To send get request
 router.get("/", (req, res, next) => { 
@@ -15,6 +17,11 @@ router.get("/", (req, res, next) => {
     // var payload = {
     //     pageTitle: "Home"
     // }
+    res.status(200).render("register");
+})
+// To move forward from register
+router.post("/", (req, res, next) => { 
+    console.log(req.body);
     res.status(200).render("register");
 })
 
